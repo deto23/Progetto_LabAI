33c5a70940121153f8d660694dba6cebea30907a
deto23 <97342146+deto23@users.noreply.github.com>
2022-07-14 18:53:27 +0200

    Update app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -24,11 +24,26 @@ def home():
     
     return render_template("home.html")
 
+@app.route("/hello", methods=['POST','GET'])
+def hello():
+    try:
+        message = request.data
+        return do_something(message)
+    except Exception:
+        return  render_template('hello.html', message = 'Error')
+
+
+def do_something(message):
+    with open('messages.log', 'a') as f:
+        current_time = time.ctime()
+        f.write(current_time + ': ' + message + '\n')
+    
+    return  render_template('hello.html', message = message)
+
  
 @app.route("/about")
 def about():
     return render_template("about.html")
-
 
 @app.route('/upload')
 def upload():
@@ -37,23 +52,34 @@ def upload():
 @app.route('/uploader')
 def upload_file():
     url = "http://192.168.1.164:8080/shot.jpg"
+
+    temp = True
   
     # While loop to continuously fetching data from the Url
-    while True:
+    while temp:
         img_resp = requests.get(url)
-        
         img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
         img = cv2.imdecode(img_arr, -1)
-        img = imutils.resize(img, width=1000, height=1800)
+
+        img = imutils.rotate(img,-90)
+        img = imutils.resize(img, height = 1920, width=1080)
         cv2.imshow("Android_cam", img)
+
+        
+
     
-        # Press Esc key to exit
         if cv2.waitKey(1) == ord('s'):
             cv2.imwrite(filename='static/image/saved_img.jpg', img=img)
             print("Image saved!")
+            temp = False
 
-            break
+        elif cv2.waitKey(1)  == 27:
+            temp = False
+    
+    cv2.destroyAllWindows()
+
     return render_template('upload.html')
+
 
 @app.route('/show_img')
 def show_img():
@@ -69,7 +95,6 @@ def show_text():
     pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
     text = pytesseract.image_to_string(img1)
     return render_template("show_text.html", user_text = text)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
