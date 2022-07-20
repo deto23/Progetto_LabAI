7afa5cbdb57c404a4c85af31a5dfa63f3da95a3e
deto23 <97342146+deto23@users.noreply.github.com>
2022-07-20 11:48:11 +0200

    Delete script.js

--- flask-app/static/css/script/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('ul.navbar-nav > li').click(function(e) {
-      e.preventDefault();
-      $('ul.navbar-nav > li').removeClass('active');
-      $(this).addClass('active');
-    });
-});
